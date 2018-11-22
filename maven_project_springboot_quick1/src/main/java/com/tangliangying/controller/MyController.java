e45db3cf0901fa1b0b8e341630227c5e7b4cfb6b
tangliangying <ly_tang_index@163.com>
2018-11-22 22:36:58 +0800

    修改3

---
+++ maven_project_springboot_quick1/src/main/java/com/tangliangying/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.tangliangying.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @RequestMapping("/hello")
+    public String helloStringBoot(){
+        return "hello.SpringBoo1231231231t";
+    }
+
+}
